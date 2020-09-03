54f38a3e0df611317822c25cd3c7bc6cfcc68912
Jos Craw <jos@joscraw.net>
2020-09-03 20:14:11 +1200

    Added extra tests

--- testbenches/new/alu_testbench.vhd
+++ testbenches/new/alu_testbench.vhd
@@ -102,13 +102,92 @@ begin
         assert (output = X"00000000") report "Incorrect Result" severity failure;
         
         
-        in_a <= X"00000008";
-        in_b <= X"00000008";
+        in_a <= X"0000000F";
+        in_b <= X"00000001";
         instruction <= X"00";
         
         wait for CLK_period;
         assert (output = X"00000010") report "Incorrect Result" severity failure;
         
+        
+        -- OR
+       --------------------------------------------------------------------------
+
+        in_a <= X"0000000F";
+        in_b <= X"00000001";
+        instruction <= X"01";
+        
+        wait for CLK_period;
+        assert (output = X"0000000F") report "Incorrect Result" severity failure;
+    
+        
+        in_a <= X"00000008";
+        in_b <= X"00000008";
+        instruction <= X"01";
+        wait for CLK_period;
+        assert (output = X"00000008") report "Incorrect Result" severity failure;
+        
+        in_a <= X"0F0F0F0F";
+        in_b <= X"F0F0F0F0";
+        instruction <= X"01";
+        
+        wait for CLK_period;
+        assert (output = X"FFFFFFFF") report "Incorrect Result" severity failure;
+        
+        
+        in_a <= X"0000000F";
+        in_b <= X"000000F0";
+        instruction <= X"01";
+        
+        wait for CLK_period;
+        assert (output = X"000000FF") report "Incorrect Result" severity failure;
+        
+        
+        in_a <= X"00000003";
+        in_b <= X"0000000C";
+        instruction <= X"01";
+        wait for CLK_period;
+        assert (output = X"0000000F") report "Incorrect Result" severity failure;
+
+        -- AND
+       --------------------------------------------------------------------------
+
+        in_a <= X"0000000F";
+        in_b <= X"0000000F";
+        instruction <= X"02";
+        
+        wait for CLK_period;
+        assert (output = X"0000000F") report "Incorrect Result" severity failure;
+    
+        
+        in_a <= X"00000003";
+        in_b <= X"0000000C";
+        instruction <= X"02";
+        wait for CLK_period;
+        assert (output = X"00000000") report "Incorrect Result" severity failure;
+        
+        in_a <= X"0F0F0F0F";
+        in_b <= X"F0F0F0F0";
+        instruction <= X"02";
+        
+        wait for CLK_period;
+        assert (output = X"00000000") report "Incorrect Result" severity failure;
+        
+        
+        in_a <= X"FFFFFFFF";
+        in_b <= X"FFFFFFFF";
+        instruction <= X"02";
+        
+        wait for CLK_period;
+        assert (output = X"FFFFFFFF") report "Incorrect Result" severity failure;
+        
+        
+        in_a <= X"000000FC";
+        in_b <= X"000000FC";
+        instruction <= X"02";
+        wait for CLK_period;
+        assert (output = X"000000FC") report "Incorrect Result" severity failure;
+
     assert (false) report "Tests Completed Successfully" severity failure;
     end process;
 
